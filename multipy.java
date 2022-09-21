9bd275448ad3cdfdd2c8383cd148a37e29aef114
Resilian <91998206+Resilian@users.noreply.github.com>
2022-09-21 10:23:03 -0700

    Create multipy.java
    working in progress

---
+++ multipy.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+
+public class call {
+    public void print( String args[] ) {
+        Random random = new Random();
+        System.out.println("A random int: " + random.nextInt()); 
+    } 
+}
